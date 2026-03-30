1b4587e2942efa1630fade66c4d5f01ed7b60264
unknown <Student@LAB4V2K4.et.put.poznan.pl>
2026-03-30 10:40:59 +0200

    Dodanie testu sumujacego

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -30,3 +30,11 @@ def test_if_tenants_have_valid_apartment_keys():
 
     manager.tenants['tenant-1'].apartment = 'invalid-key'
     assert manager.check_tenants_apartment_keys() == False
+    
+def test_apartment_costs():
+    parameters = Parameters()
+    manager = Manager(parameters)
+    
+    assert manager.get_apartment_costs('apart-polanka', 2025, 1) == 910.0
+    assert manager.get_apartment_costs('apart-polanka', 2025, 2) == 0.0
+    assert manager.get_apartment_costs('apart-biskupice', 2025, 1) == None
